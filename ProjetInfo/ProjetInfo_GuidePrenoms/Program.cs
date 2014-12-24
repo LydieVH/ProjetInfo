c2ec5ebce7349152dd914b749f93d78342ed2cc7
LydieVH <lvanhoevela@ensc.fr>
2014-12-24 16:15:40 +0100

    Premiere ebauche
    Début du code, il faut choisir si on créer notre propre structure ou si
    on fait un tableau de tableau !

--- ProjetInfo/ProjetInfo_GuidePrenoms/Program.cs
+++ ProjetInfo/ProjetInfo_GuidePrenoms/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace ProjetInfo_GuidePrenoms
 {
@@ -10,6 +11,23 @@ namespace ProjetInfo_GuidePrenoms
     {
         static void Main(string[] args)
         {
+            // Chargement du fichier 
+            public static void copieFichierDansTableau (string fichierSource)
+            {
+                try 
+                {
+                    // Création d'une instance StreamReader pour notre fichier
+                    System.Text.Encoding   encoding = System.Text.Encoding.GetEncoding(  "iso-8859-1"  );
+                    StreamReader monStreamReader = new StreamReader(fichierSource,encoding); 
+                    string ligne = monStreamReader.ReadLine();
+
+                    // Copie du fichier texte dans un tableau 
+                    while (ligne != null)
+                    {
+                        // IL FAUT CHOISIR SI ON FAIT UN TABLEAU DE TABLEAU OU SI ON CREER NOTRE PROPRE STRUCTURE
+                    }
+                }
+            }
         }
     }
 }
