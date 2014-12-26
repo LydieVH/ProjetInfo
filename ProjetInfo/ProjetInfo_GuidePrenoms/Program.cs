33513e37b05716618e5116d6b1ef3b59573accd4
gandhi33 <alexis.deny@gmail.com>
2014-12-26 15:17:38 +0100

    Structure
    J'ai commencé la structure

--- ProjetInfo/ProjetInfo_GuidePrenoms/Program.cs
+++ ProjetInfo/ProjetInfo_GuidePrenoms/Program.cs
@@ -10,24 +10,33 @@ namespace ProjetInfo_GuidePrenoms
     class Program
     {
         static void Main(string[] args)
-        {
+        { }
             // Chargement du fichier 
-            public static void copieFichierDansTableau (string fichierSource)
+        public static void copieFichierDansTableau (string fichierSource)
+        {
+            try
             {
-                try 
-                {
-                    // Création d'une instance StreamReader pour notre fichier
-                    System.Text.Encoding   encoding = System.Text.Encoding.GetEncoding(  "iso-8859-1"  );
-                    StreamReader monStreamReader = new StreamReader(fichierSource,encoding); 
-                    string ligne = monStreamReader.ReadLine();
+                // Création d'une instance StreamReader pour notre fichier
+                Console.WriteLine("\nChargement du fichier : " + fichier);
+                System.Text.Encoding   encoding = System.Text.Encoding.GetEncoding(  "iso-8859-1"  );
+                StreamReader monStreamReader = new StreamReader(fichierSource,encoding); 
+                string ligne = monStreamReader.ReadLine();
 
-                    // Copie du fichier texte dans un tableau 
-                    while (ligne != null)
-                    {
-                        // IL FAUT CHOISIR SI ON FAIT UN TABLEAU DE TABLEAU OU SI ON CREER NOTRE PROPRE STRUCTURE
-                    }
+                // Copie du fichier texte dans un tableau 
+                while (ligne != null)
+                {
+                   // IL FAUT CHOISIR SI ON FAIT UN TABLEAU DE TABLEAU OU SI ON CREER NOTRE PROPRE STRUCTURE
                 }
             }
+                catch {}
+        }
+        
+        
+        public struct guidePrenoms
+        {
+            public int annee, nb;
+            public string prenom;
+            public int ordre;
         }
     }
 }
