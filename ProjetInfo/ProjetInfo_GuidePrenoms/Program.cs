69177f280e3e8e0ad8751971ddcf1f0d85d56387
LydieVH <lvanhoevela@ensc.fr>
2015-01-05 14:58:30 +0100

    Premiere partie
    Gestion du fichier et du tableau

--- ProjetInfo/ProjetInfo_GuidePrenoms/Program.cs
+++ ProjetInfo/ProjetInfo_GuidePrenoms/Program.cs
@@ -9,7 +9,7 @@ namespace ProjetInfo_GuidePrenoms
 {
     class Program
     {
-        struct entite
+        public struct entite
         {
             public int annee;
             public string prenom;
@@ -17,34 +17,55 @@ namespace ProjetInfo_GuidePrenoms
             public int rang;
         }
         // Chargement du fichier 
-        public static void copieFichierDansTableau (string fichierSource)
+        public static void copieFichierDansTableau (out entite[] entites)
         {
-            string[] lignes = System.IO.File.ReadAllLines(fichierSource);
-            entite[] entites = new entite[lignes.Length-1];  // -1 car on prend pas la première ligne du fichier 
-            for (int i = 1; i < lignes.Length; i++)
+            try
             {
-                string[] ligneDecoupee = lignes[i].Split();
-                entites[i-1].annee = int.Parse(ligneDecoupee[0]);   // i-1 car on n'a pas pris la première ligne du fichier..
-                entites[i-1].prenom = ligneDecoupee[1];             // .. et sinon on dépassera de notre tableau
-                entites[i-1].nbDeFoisDonne = int.Parse(ligneDecoupee[2]);
-                entites[i-1].rang = int.Parse(ligneDecoupee[3]);
+                string[] lignes = System.IO.File.ReadAllLines("prenoms_bordeaux.txt");
+                entites = new entite[lignes.Length-1];  // -1 car on prend pas la première ligne du fichier 
+                for (int i = 1; i < lignes.Length; i++)
+                {
+                    string[] ligneDecoupee = lignes[i].Split();
+                    entites[i-1].annee = int.Parse(ligneDecoupee[0]);   // i-1 car on n'a pas pris la première ligne du fichier..
+                    entites[i-1].prenom = ligneDecoupee[1];             // .. et sinon on dépassera de notre tableau
+                    entites[i-1].nbDeFoisDonne = int.Parse(ligneDecoupee[2]);
+                    entites[i-1].rang = int.Parse(ligneDecoupee[3]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("/!\\ Veuillez vérifier que le fichier \"prenoms_bordeaux.txt\" soit dans le même dossier que le fichier .exe de l'application.\nTaper \"ok\" lorsque l'opération est effectuée.");
+                while (string.Equals(Console.ReadLine().ToUpper(), "OK") == false)
+                    Console.WriteLine("/!\\ Veuillez vérifier que le fichier \"prenoms_bordeaux.txt\" soit dans le même dossier que le fichier .exe de l'application.\nTaper \"ok\" lorsque l'opération est effectuée.");
+                copieFichierDansTableau(out entites);
             }
         }
 
-        public static void afficheFichier(entite[] entites)
+        public static void afficheFichier(entite[] entites) 
         {
             int longueur = entites.Length, i = 0;
-            while (i < longueur)
+            while (i < longueur)    // Gestion de l'alignement des colonnes
             {
-                Console.WriteLine("{0}\t{1}\t{2}\t{3}", entites[i].annee, entites[i].prenom, entites[i].nbDeFoisDonne, entites[i].rang);
-                i++;
+                if (entites[i].prenom.Length > 7) 
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", entites[i].annee, entites[i].prenom, entites[i].nbDeFoisDonne, entites[i].rang);
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("{0}\t{1}\t\t{2}\t{3}", entites[i].annee, entites[i].prenom, entites[i].nbDeFoisDonne, entites[i].rang);
+                    i++;
+                }
             }
-        }
+        } 
+
 
         static void Main(string[] args)
         {
-            copieFichierDansTableau(@"C:\Users\LVH2\Documents\ENSC\PROG\PROJET");
-            afficheFichier(entites[88]); // <-- on peut pas recup le tableau crée dans la fct° copieFichierDansTableau, il a été effacé dans la pile à la fin de la fonction non ? 
+            entite[] entites;
+            copieFichierDansTableau(out entites);
+            afficheFichier(entites);
+ 
             Console.ReadLine();
         }
     }
